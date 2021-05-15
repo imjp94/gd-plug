67e6982d011a7e9cfa007ab3e22a76256ae8f0e9
imjp94 <imjp_94@hotmail.com>
2021-05-15 15:48:50 +0800

    Add Logger

--- addons/gd-plug/plug.gd
+++ addons/gd-plug/plug.gd
@@ -470,3 +470,50 @@ class GitExecutable extends Reference:
 		for msg in raw_ahead_behind:
 			ahead_behind.append(int(msg))
 		return ahead_behind if exit == OK else []
+
+class Logger extends Reference:
+	enum LogLevel {
+		ALL, DEBUG, INFO, WARN, ERROR, NONE
+	}
+	const DEFAULT_LOG_FORMAT_DETAIL = "[{time}] [{level}] {msg}"
+	const DEFAULT_LOG_FORMAT_NORMAL = "{msg}"
+	
+	var log_level = LogLevel.INFO
+	var log_format = DEFAULT_LOG_FORMAT_NORMAL
+	var log_time_format = "{year}/{month}/{day} {hour}:{minute}:{second}"
+
+	func debug(msg, raw=false):
+		_log(LogLevel.DEBUG, msg, raw)
+
+	func info(msg, raw=false):
+		_log(LogLevel.INFO, msg, raw)
+
+	func warn(msg, raw=false):
+		_log(LogLevel.WARN, msg, raw)
+
+	func error(msg, raw=false):
+		_log(LogLevel.ERROR, msg, raw)
+
+	func _log(level, msg, raw=false):
+		if log_level <= level:
+			if raw:
+				printraw(format_log(level, msg))
+			else:
+				print(format_log(level, msg))
+
+	func format_log(level, msg):
+		return log_format.format({
+			"time": log_time_format.format(get_formatted_datatime()),
+			"level": LogLevel.keys()[level],
+			"msg": msg
+		})
+
+	func get_formatted_datatime():
+		var datetime = OS.get_datetime()
+		datetime.year = "%04d" % datetime.year
+		datetime.month = "%02d" % datetime.month
+		datetime.day = "%02d" % datetime.day
+		datetime.hour = "%02d" % datetime.hour
+		datetime.minute = "%02d" % datetime.minute
+		datetime.second = "%02d" % datetime.second
+		return datetime
